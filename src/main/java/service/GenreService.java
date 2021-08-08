8053f47656927f431b432fc74a7825d34ab0b88c
Pastornak <yurapaster@gmail.com>
2021-08-08 23:20:35 +0300

    Update GenreService with the new usage of BaseResponse

--- src/main/java/service/GenreService.java
+++ src/main/java/service/GenreService.java
@@ -7,12 +7,12 @@ import utils.EndpointBuilder;
 
 public class GenreService {
 
-    public BaseResponse getGenre(int genreId) {
+    public BaseResponse<Object> getGenre(int genreId) {
         String endpoint = new EndpointBuilder().pathParameter("genre").pathParameter(genreId).get();
-        return HttpClient.get(endpoint);
+        return new BaseResponse<>(HttpClient.get(endpoint), Object.class);
     }
 
-    public BaseResponse getGenres(ListOptions options) {
+    public BaseResponse<Object> getGenres(ListOptions options) {
         EndpointBuilder endpoint = new EndpointBuilder().pathParameter("genres");
         if (options.orderType != null) endpoint.queryParam("orderType", options.orderType);
         endpoint
@@ -20,12 +20,12 @@ public class GenreService {
             .queryParam("pagination", options.pagination)
             .queryParam("size", options.size);
         if (options.sortBy != null) endpoint.queryParam("sortBy", options.sortBy);
-        return HttpClient.get(endpoint.get());
+        return new BaseResponse<>(HttpClient.get(endpoint.get()), Object.class);
     }
 
     // TODO properly handle genre entity
-    public BaseResponse createGenre(Object genre) {
+    public BaseResponse<Object> createGenre(Object genre) {
         String endpoint = new EndpointBuilder().pathParameter("genre").get();
-        return HttpClient.post(endpoint, genre.toString());
+        return new BaseResponse<>(HttpClient.post(endpoint, genre.toString()), Object.class);
     }
 }
