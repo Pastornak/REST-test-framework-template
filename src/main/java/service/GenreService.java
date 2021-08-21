4aff649742549f7abaa6a5895166545cc7486e79
Pastornak <yurapaster@gmail.com>
2021-08-21 22:04:26 +0300

    Update GenreService implementation to use client from ServiceConfig

--- src/main/java/service/GenreService.java
+++ src/main/java/service/GenreService.java
@@ -1,31 +1,47 @@
 package service;
 
-import client.HttpClient;
+import com.google.gson.Gson;
+import config.ServiceConfig;
+import entity.Genre;
 import entity.ListOptions;
-import response.BaseResponse;
+import response.*;
+import response.genre.GenreResponse;
+import response.genre.GenresResponse;
 import utils.EndpointBuilder;
 
-public class GenreService {
+import java.util.Arrays;
 
-    public BaseResponse<Object> getGenre(int genreId) {
+public class GenreService {
+    private static Gson g = new Gson();
+
+    public GenreResponse getGenre(int genreId) {
         String endpoint = new EndpointBuilder().pathParameter("genre").pathParameter(genreId).get();
-        return new BaseResponse<>(HttpClient.get(endpoint), Object.class);
+        return new GenreResponse(ServiceConfig.CLIENT.get(endpoint));
     }
 
-    public BaseResponse<Object> getGenres(ListOptions options) {
-        EndpointBuilder endpoint = new EndpointBuilder().pathParameter("genres");
+    public GenresResponse getGenres(ListOptions options) {
+        EndpointBuilder endpoint = new EndpointBuilder().pathParameter("genre");
         if (options.orderType != null) endpoint.queryParam("orderType", options.orderType);
         endpoint
             .queryParam("page", options.page)
             .queryParam("pagination", options.pagination)
             .queryParam("size", options.size);
         if (options.sortBy != null) endpoint.queryParam("sortBy", options.sortBy);
-        return new BaseResponse<>(HttpClient.get(endpoint.get()), Object.class);
+        return new GenresResponse(ServiceConfig.CLIENT.get(endpoint.get()));
     }
 
-    // TODO properly handle genre entity
-    public BaseResponse<Object> createGenre(Object genre) {
+    public GenreResponse createGenre(Genre genre) {
         String endpoint = new EndpointBuilder().pathParameter("genre").get();
-        return new BaseResponse<>(HttpClient.post(endpoint, genre.toString()), Object.class);
+        return new GenreResponse(ServiceConfig.CLIENT.post(endpoint, Arrays.asList(new Header("content-type", "application/json")), g.toJson(genre)));
+    }
+
+    public GenreResponse updateGenre(Genre genre) {
+        String endpoint = new EndpointBuilder().pathParameter("genre").get();
+        return new GenreResponse(ServiceConfig.CLIENT.put(endpoint, Arrays.asList(new Header("content-type", "application/json")), g.toJson(genre)));
+    }
+
+    public BaseResponse deleteGenre(int genreId) {
+        String endpoint = new EndpointBuilder().pathParameter("genre").pathParameter(genreId).get();
+        return ServiceConfig.CLIENT.delete(endpoint);
     }
 }
