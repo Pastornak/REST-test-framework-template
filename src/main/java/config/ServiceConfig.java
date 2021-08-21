6d16e00d58641fc9e2aad11a17c64b0462115b3f
Pastornak <yurapaster@gmail.com>
2021-08-21 22:03:47 +0300

    Keep HttpClient implementation as static variable in ServiceConfig

--- src/main/java/config/ServiceConfig.java
+++ src/main/java/config/ServiceConfig.java
@@ -1,9 +1,13 @@
 package config;
 
+import client.IHttpClient;
+import client.RestAssuredClient;
+import client.RestTemplateClient;
 import utils.PropertiesReader;
 
 public class ServiceConfig {
     public static final String HOST = ServiceConfig.getHost();
+    public static final IHttpClient CLIENT = new RestTemplateClient();
 
     private static String getHost() {
         return new PropertiesReader("config.properties").get("host");
